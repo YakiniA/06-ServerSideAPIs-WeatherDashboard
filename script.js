3e8f2f8ee26c7cb8ee291bcc819d9588ee02843d
YakiniA <yakinilatha09@gmail.com>
2020-06-28 23:27:56 -0600

    Added search functionality

---
+++ script.js
@@ -0,0 +1,43 @@
+function WeatherDashboard() {
+
+    $(document).ready(function () {
+
+        $(".submitBtn").on("click", function(event){
+            
+            event.preventDefault();
+            console.log("Got the click");
+            var value = $(this).siblings("input").val().trim();
+            console.log(value);
+            displayDetails(value);
+        
+            
+
+        });
+});
+}
+
+function displayDetails(value){
+var searchHistory;
+var div;
+if(localStorage.getItem("searchHistory")){
+    searchHistory =  JSON.parse(localStorage.getItem("searchHistory"));
+   
+}else{
+    searchHistory = [];
+}
+  if((value!=null ) && (value!="" )){   
+    searchHistory.push(value);
+  
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    
+}
+
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+
+div = $("<div>").text(searchHistory);
+
+ $(".searchHistoryCity").append(div);
+}
+
+WeatherDashboard();
+displayDetails();
