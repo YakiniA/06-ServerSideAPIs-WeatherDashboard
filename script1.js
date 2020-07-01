de6842864d6d93a9a16e8f8da2e7e0e2d1383ff9
YakiniA <yakinilatha09@gmail.com>
2020-06-30 22:39:20 -0600

    Adding more features

--- script1.js
+++ script1.js
@@ -1,9 +1,16 @@
 var value = "";
+var weatherDetails = $(".weather-details");
+var cityDetails = $(".cityDetails");
+var temperature = $(".temperature");
+var humidity = $(".humidity");
+var windSpeed = $(".wind-speed");
+var uvIndex = $(".uvIndex");
+
 function WeatherDashboard() {
 
     $(document).ready(function () {
 
-        $(".submitBtn").on("click", function(event){
+        $("#submitBtn").on("click", function(event){
             
             event.preventDefault();
             console.log("Got the click");
@@ -34,44 +41,63 @@ function displayDetails(value){
         }
 }
 
-        var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
-        if(searchHistory!=null){
-            for(var values= 0; values<searchHistory.length; values++){
+    //     var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    //     if(searchHistory!=null){
+    //         for(var values= 0; values<searchHistory.length; values++){
              
-                if(value!= searchHistory[values]){
-                var pCityName = $("<p style='margin:16px 0; font-size:18px;'>");
-                pCityName.text(searchHistory[values]);
-                $(".searchHistoryCity").append(pCityName);
-                }
-            }
-    }
+    //             if(searchHistory[values]!= searchHistory[values+1]){
+    //             var pCityName = $("<p style='margin:16px 0; font-size:18px;'>");
+    //             pCityName.text(searchHistory[values]);
+    //             $(".searchHistoryCity").append(pCityName);
+    //             }
+    //         }
+    // }
+
+    $("#sampleSearch button").on( "click", function(event) {
+        event.preventDefault();
+        value =$(this).html().toString();
+        console.log("Value" +value);
+        displayDetails(value);
+        displayWeatherDetails(value);
+    });
 
     function displayWeatherDetails(value){
         // "https://api.openweathermap.org/data/2.5/weather?" +
         // "q=Bujumbura,Burundi&appid=" + APIKey  5a9ba99aa270a73fe708b6e2422c838d;
         var tempF;
-        var humidity;
+        var humidityVal;
         var speed;
         var icon;
         var lat;
         var long;
+        var date;
+        var uvIndexVal;
+        var wf = "";
         var APIKey="5a9ba99aa270a73fe708b6e2422c838d";
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +"q=" +value +"&appid=" + APIKey;
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +"q=" +value +"&appid=" + APIKey +"&units=5";
        
          $.ajax({
           url: queryURL,
-          method: "GET"
+          method: "GET",
+          
         }).then(function(response) {    
 
-           
           console.log(JSON.stringify(response));  
-          tempF = (response.main.temp - 273.15) * 1.80 + 32;
-          humidity = response.main.humidity;
+          tempF = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+          humidityVal = response.main.humidity;
           speed = response.wind.speed;
-        //   console.log("Speed" +speed);
           icon = response.weather[0].icon;
           long = response.coord.lon;
           lat = response.coord.lat;
+
+          $.each(response.list, function(index, val) {
+            wf += "<p>" // Opening paragraph tag
+            wf += "<b>Day " + index + "</b>: " // Day
+            wf += val.main.temp + "&degC" // Temperature
+            wf += "<span> | " + val.weather[0].description + "</span>"; // Description
+            wf += "<img src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'>" // Icon
+            wf += "</p>" // Closing paragraph tag
+          });
                    
             var queryURL1 = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + APIKey+ "&lat="+lat+ "&lon=" +long;
 
@@ -80,19 +106,31 @@ function displayDetails(value){
                 method: "GET"
             }).then(function(response) {  
                 console.log(JSON.stringify(response));
-                var date = response.date_iso;
-                var uvIndex = response.value;
-        
+                date = response.date;
+                uvIndexVal = response.value;
+              
             });
-          
+         var iconimg = "http://openweathermap.org/img/w/" + icon + ".png";
+         var city = response.name  ;
+         var city = $(".city").text(city);
+         var date =  $(".date").text(moment(date).format("MM/DD/YYYY"));
+         var iconImg = $("#icon").attr("src",iconimg);
+
+         cityDetails.append(city).append(date).append(iconImg);
+       
+         temperature.text("Temperature : " +tempF);
+         humidity.text("Humidity : " +humidityVal);
+         windSpeed.text("Wind Speed : " +speed);
+         uvIndex.text("UV Index : " +uvIndexVal);
+
+         weatherDetails.append(cityDetails);
+         weatherDetails.append(temperature);
+         weatherDetails.append(humidity);
+         weatherDetails.append(windSpeed);
+         weatherDetails.append(uvIndex);
+       
       });
 
-      console.log(speed);
-      console.log(humidity);
-      console.log(tempF);
-      console.log(lat);
-      console.log(long);
-     
     }
 
 WeatherDashboard();
